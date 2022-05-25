e883fc820cc2e8575219d03e5814a6c701cd8ce4
syhirrh <syasyahirah31@gmail.com>
2022-05-26 00:19:18 +0800

    third commit

--- conn.php
+++ conn.php
@@ -6,7 +6,7 @@ define('db_name','registration');
 
 $conn = mysqli_connect (db_host, db_user, db_password, db_name);
 
-if (mysqli_connect_errno())
+if (mysqli_connect_error())
 {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
