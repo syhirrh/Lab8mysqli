4a367fd392d2d7d11cec77837d2d4fe71f1cc7b6
syhirrh <syasyahirah31@gmail.com>
2022-05-25 23:36:15 +0800

    first commit

---
+++ add_user.php
@@ -0,0 +1,13 @@
+<?php
+include 'conn.php';
+
+$sql="insert into user (first_name, last_name, email, pass_word)
+values ('$_POST[firstname]','$_POST[lastname]','$_POST[email]',
+'$_POST[password]','$_POST[reg_date]')";
+
+    if(!mysqli_query($conn, $sql)){
+        die('Error: '.mysqli_error());
+    }
+    echo "1 record successfully added... ";
+
+?>
