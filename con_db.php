8cfd7be5a8768752ba702d9a3997701879421365
syhirrh <syasyahirah31@gmail.com>
2022-05-25 22:48:17 +0800

    first commot

--- con_db.php
+++ con_db.php
@@ -5,7 +5,7 @@ define('db_host','localhost');
 define('db_name','registration');
 
 $conn = mysqli_connect (db_host, db_user, db_password, db_name);
-$result = myqli_query($conn, "select * from user");
+$result = mysqli_query($conn, "select * from user");
 while($row = mysqli_fetch_array($result)){
         foreach($row as $key=>$value){
             echo "$key = $value <br>\n";
